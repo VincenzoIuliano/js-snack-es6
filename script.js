dce42ae22dd594685f5c6371a8fbabbbe5ced97d
VincenzoIuliano <bubucane@outlook.it>
2024-10-25 15:37:16 +0200

    ADD: snack 1

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Object Snack')
+
+let bikes = [
+    {
+        'name': 'Decathlon',
+        'weight': 10,
+    },
+    {
+        'name': 'Cisalfa',
+        "weight": 15,
+    },
+]
+
+console.log(bikes)
