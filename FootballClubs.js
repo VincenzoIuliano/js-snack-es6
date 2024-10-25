0eb0ff22736c4137b22f37b833923f482d3cbf47
VincenzoIuliano <bubucane@outlook.it>
2024-10-25 15:44:36 +0200

    ADD: snack 2 - created array

---
+++ FootballClubs.js
@@ -0,0 +1,28 @@
+console.log('Object Snack')
+
+// CREO UN ARRAY CONTENTE COME OGGETTI DELLE SQUADRE DI CALCIO CON DELLE CARATTERISTICHE CHE ASSEGNO SUCCESSIVAEMNTE
+
+let footballClubs = [
+    {
+        'nome' : 'Inter',
+        'punti fatti' : 0 ,
+        'falli subiti' : 0,
+    },
+    {
+        'nome' : 'Milan',
+        'punti fatti' : 0,
+        'falli subiti' : 0,
+    },
+    {
+        'nome' : 'Juventus',
+        'punti fatti' : 0,
+        'falli subiti' : 0,
+    },
+    {
+        'nome' : 'Napoli',
+        'punti fatti' : 0,
+        'falli subiti' : 0,
+    },
+]
+
+console.log(footballClubs)
