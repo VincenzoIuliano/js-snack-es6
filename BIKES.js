97dc157bd9b048a74e7ee064b3e2d905f7ae1f0e
VincenzoIuliano <bubucane@outlook.it>
2024-10-26 11:38:07 +0200

    ADD: snack 2 correction

--- BIKES.js
+++ BIKES.js
@@ -1,16 +1,35 @@
 console.log('Object Snack')
 
-// CREO UN ARRAY CHE CONTENGA DUE OGGETTI: OGNI OGGETTO E' UNA BICI DA CORSA
+// CREO UN ARRAY DI OGGETT: OGNI OGGETTO E' UNA BICI DA CORSA
 
 let bikes = [
     {
         'name': 'Decathlon',
-        'weight': 10,
+        'weight' : 4,
     },
     {
-        'name': 'Cisalfa',
-        "weight": 15,
+       ' name': 'Cisalfa',
+        'weight' : 20,
+    },
+    {
+        'name' : 'Nike',
+        'weight' : 10,
     },
 ]
 
-console.log(bikes)
+// console.log(bikes)
+
+// peso di riferimento, per esempio prima bicicletta
+let biciLeggera = bikes[0]
+// console.log(biciLeggera)
+// ciclo per scorrere tutti gli elementi dell'array
+for (let i = 0; i < bikes.length; i++) {
+    const elementoCorrente = bikes[i]
+    // console.log(elementoCorrente)
+    if (elementoCorrente.weight < biciLeggera.weight) {
+        biciLeggera=elementoCorrente
+    } 
+}
+
+console.log(biciLeggera)
+// condizione che verifichi se il riferimento è da aggiornare - confronto riferimento con peso elemento i
