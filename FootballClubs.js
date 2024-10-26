a1ed0fda6456ffeca8581da053b3c9f2f5e7a190
VincenzoIuliano <bubucane@outlook.it>
2024-10-26 11:24:05 +0200

    ADD: new array solution

--- FootballClubs.js
+++ FootballClubs.js
@@ -5,31 +5,40 @@ console.log('Object Snack')
 let footballClubs = [
     {
         'nome' : 'Inter',
-        'punti fatti' : 0 ,
-        'falli subiti' : 0,
+        'puntifatti' : 0 ,
+        'fallisubiti' : 0,
     },
     {
         'nome' : 'Milan',
-        'punti fatti' : 0,
-        'falli subiti' : 0,
+        'puntifatti' : 0,
+        'fallisubiti' : 0,
     },
     {
         'nome' : 'Juventus',
-        'punti fatti' : 0,
-        'falli subiti' : 0,
+        'puntifatti' : 0,
+        'fallisubiti' : 0,
     },
     {
         'nome' : 'Napoli',
-        'punti fatti' : 0,
-        'falli subiti' : 0,
+        'puntifatti' : 0,
+        'fallisubiti' : 0,
     },
 ]
 
 // ASSEGNO NUMERI RANDOM AI PARAMENTRI A 0
 
 for (let i=0 ; i < footballClubs.length ; i++) {
-    footballClubs[i]["punti fatti"]=Math.round(Math.random()*100 + 1);
-    footballClubs[i]["falli subiti"]=Math.round(Math.random()*100 + 1);
+    footballClubs[i]["puntifatti"]=Math.round(Math.random()*100 + 1);
+    footballClubs[i]["fallisubiti"]=Math.round(Math.random()*100 + 1);
 }
 
 console.log(footballClubs)
+
+let newArray = []
+
+for (let i = 0; i < footballClubs.length; i++) {
+    newArray.push(footballClubs[i].nome);
+    newArray.push(footballClubs[i].puntifatti);
+}
+
+console.log(newArray)
